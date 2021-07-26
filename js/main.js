eab96fb4b0ec9af1449aedaa9e993be9db8ec16c
Henrique Lopes <henrique.l.vieira92@gmail.com>
2021-07-26 20:12:29 -0300

    Aplicação ISOTOPE

--- js/main.js
+++ js/main.js
@@ -9,5 +9,27 @@ jQuery(document).ready(function($) {
         }
 
     }
+
+    let btns = $("#servicos .button-group button");
+
+    btns.click(function (e) {
+        $("#servicos .button-group button").removeClass('active');
+        e.target.classList.add('active');
+
+
+        let selector = $(e.target).attr("data-filter");
+        $("#servicos .grid").isotope({
+            filter: selector,
+        });
+
+    });
+
+    $(window).on("load", function () {
+        $("#servicos .grid").isotope({
+            filter: "*",
+        });
+    })
+
+
 }
 );
